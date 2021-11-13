355266fea742abee3ace3a095bf29503209325c5
Marc Mendoza <777marc@gmail.com>
2021-11-13 06:03:24 -0500

    wrap up error handling base

---
+++ auth/src/errors/not-found-error.ts
@@ -0,0 +1,16 @@
+import { CustomError } from "./custom-error";
+
+export class NotFoundError extends CustomError {
+    statusCode = 404;
+
+    constructor() {
+        super('Route Not Found');
+
+        Object.setPrototypeOf(this, NotFoundError);
+    }
+
+    serializeErrors() {
+        return [{ message: 'Not Found'}];
+    }
+
+}
