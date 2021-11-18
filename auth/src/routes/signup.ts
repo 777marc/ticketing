e8602f39b5daa58fc106a400d59f4566d975c0a7
Marc Mendoza <777marc@gmail.com>
2021-11-18 09:30:51 -0500

    misc

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -43,7 +43,7 @@ router.post('/api/users/signup', [
         const userJWT = jwt.sign({
             id: user.id,
             email: user.email
-        }, 'asdf');
+        }, process.env.JWT_KEY!);
 
         // store on session object
         req.session = {
