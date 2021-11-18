c77f0c62432b54552b028eb730e71806886c89ec
Marc Mendoza <777marc@gmail.com>
2021-11-18 15:24:59 -0500

    more middleware

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body } from 'express-validator';
 import jwt from 'jsonwebtoken';
-
 import { BadRequestError } from '../errors/bad-request-error';
 import { User } from '../models/user';
 import { validateRequest } from '../middlewares/validate-request';
@@ -20,8 +19,6 @@ router.post('/api/users/signup', [
     validateRequest,
     async (req : Request, res: Response) => {
     
-
-
         const { email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
