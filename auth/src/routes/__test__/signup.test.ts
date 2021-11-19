3edf0f0b1b4ea708cd6d4b56cd5758d752627703
Marc Mendoza <777marc@gmail.com>
2021-11-19 15:38:17 -0500

    tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -10,3 +10,41 @@ it('return a 201 on successful signup', async () => {
         })
         .expect(201);
 });
+
+it('return a 400 on bad email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'bademail',
+            password: 'password'
+        })
+        .expect(400);
+});
+
+it('return a 400 on bad password', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: ''
+        })
+        .expect(400);
+});
+
+it('disallow duplicate emails', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'passwors'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'passwors'
+        })
+        .expect(400);
+});
