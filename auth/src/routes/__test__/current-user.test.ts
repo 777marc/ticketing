9844fd8c4173d7530d88edcc6ae22bf88b5a401d
Marc Mendoza <777marc@gmail.com>
2021-11-20 13:55:40 -0500

    one more test

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -11,3 +11,10 @@ it('a valid user gets current user data', async () => {
 
     expect(response.body.currentUser.email).toEqual('test@test.com');
 });
+
+it('responds with null user data for unauthentication user', async () => {
+    await request(app)
+        .get('/api/users/currentuser')
+        .send()
+        .expect(401);
+});
