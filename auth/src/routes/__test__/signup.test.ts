99d0441f310f0dca46b3da67b226d8489b8f3792
Marc Mendoza <777marc@gmail.com>
2021-11-20 11:23:50 -0500

    more tests

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -44,7 +44,19 @@ it('disallow duplicate emails', async () => {
         .post('/api/users/signup')
         .send({
             email: 'test@test.com',
-            password: 'passwors'
+            password: 'password'
         })
         .expect(400);
+});
+
+it('sets a cookie after successful signup', async () => {
+    const response = await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    expect(response.get('Set-Cookie')).toBeDefined();
 });
