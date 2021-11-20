2a7dbde4e6ae2a2735ec9b4f7d43056a04e500c7
Marc Mendoza <777marc@gmail.com>
2021-11-20 13:11:07 -0500

    sign in and sign out tests

---
+++ auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,58 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('a valid user can sign in', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200);
+});
+
+it('an invalid user can not sign in', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'badpassword'
+        })
+        .expect(400);
+});
+
+it('a valid user gets a cookie', async () => {
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(201);
+
+    const response = await request(app)
+        .post('/api/users/signin')
+        .send({
+            email: 'test@test.com',
+            password: 'password'
+        })
+        .expect(200);
+
+    expect(response.get('Set-Cookie')).toBeDefined();        
+});
