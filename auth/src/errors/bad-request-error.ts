f3a7bbe2b8404fcffc4bda06b6cb35cf78669ad6
Marc Mendoza <777marc@gmail.com>
2021-11-22 16:14:07 -0500

    misc

--- auth/src/errors/bad-request-error.ts
+++ auth/src/errors/bad-request-error.ts
@@ -1,15 +1,14 @@
 import { CustomError } from "./custom-error";
 
 export class BadRequestError extends CustomError {
-    statusCode = 400;
+  statusCode = 400;
 
-    constructor(public message: string) {
-        super(message);
-        Object.setPrototypeOf(this, BadRequestError);
-    }
+  constructor(public message: string) {
+    super(message);
+    Object.setPrototypeOf(this, BadRequestError);
+  }
 
-    serializeErrors() {
-        return [{ message: this.message}];
-    }
-
+  serializeErrors() {
+    return [{ message: this.message }];
+  }
 }
