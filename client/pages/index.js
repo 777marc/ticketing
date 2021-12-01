d7d3cdbf531e881d4b18628dfc8dc988702e3bd3
Marc Mendoza <777marc@gmail.com>
2021-12-01 10:15:47 -0500

    landing page work

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,16 @@
-export default () => {
+import axios from "axios";
+
+const LandingPage = ({ currentUser }) => {
+  console.log("component:", currentUser);
   return <h1>landing page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  const response = await axios
+    .get("/api/users/currentuser")
+    .catch((err) => console.log("err caught", err.mesage));
+
+  return response.data;
+};
+
+export default LandingPage;
