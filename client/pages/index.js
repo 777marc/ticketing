9f587aba97b19394e801192809f6ec886b31cace
Marc Mendoza <777marc@gmail.com>
2021-12-07 16:47:52 -0500

    api work

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,16 +1,16 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
-  console.log("component:", currentUser);
-  return <h1>landing page</h1>;
+  return currentUser ? (
+    <h1>you are signed in</h1>
+  ) : (
+    <h1>you are not signed in</h1>
+  );
 };
 
-LandingPage.getInitialProps = async () => {
-  const response = await axios
-    .get("/api/users/currentuser")
-    .catch((err) => console.log("err caught", err.mesage));
-
-  return response.data;
+LandingPage.getInitialProps = async (context) => {
+  const { data } = await buildClient(context).get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
