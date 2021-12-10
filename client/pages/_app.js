2aad54a46a6961fbeba4458515be7be95ac26e96
Marc Mendoza <777marc@gmail.com>
2021-12-10 15:59:13 -0500

    adding header; misc

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
 
-export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />
+const AppComponent = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <h1>Header!</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  const { data } = await buildClient(appContext.ctx).get(
+    "/api/users/currentuser"
+  );
+  return data;
+};
+
+export default AppComponent;
